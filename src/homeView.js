e21ad4f8c8c15985c67e5476a2a383a5fbdc37e3
Robert Lewis <robertlewis@Roberts-MacBook-Pro.local>
2023-03-27 05:09:09 +0100

    feat: add banner and style to homeview

--- src/homeView.js
+++ src/homeView.js
@@ -2,6 +2,18 @@ const homeView = {
   setup() {},
   /*html*/
   template: `
-    <h1>Welcome to the Website</h1>
+
+    <q-page>
+      <q-banner class="bg-teal-200 border-teal-300 border rounded-xl m-1">
+        <div class="text-xl text-teal-700 flex flex-nowrap">
+          <i class="fa-solid fa-triangle-exclamation"></i>
+          <div class="text-base ml-2">Please note, this site is still in development some bugs maybe present.</div>
+        </div>
+      </q-banner>
+
+      <h1 class="text-center" >Welcome To the Webpage!</h1>
+
+
+    </q-page>
   `
 };

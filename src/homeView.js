f5c4d34e418d9f8c567afe57f0111965af4d50c5
Robert Lewis <robertlewis@c116-5.i11-06.melita.com>
2023-04-02 12:32:14 +0200

    rm banner from home view

--- src/homeView.js
+++ src/homeView.js
@@ -4,15 +4,7 @@ const homeView = {
   template: `
 
     <q-page>
-      <q-banner class="bg-teal-200 border-teal-300 border rounded-xl m-1">
-        <div class="text-xl text-teal-700 flex flex-nowrap">
-          <i class="fa-solid fa-triangle-exclamation"></i>
-          <div class="text-base ml-2">Please note, this site is still in development some bugs maybe present.</div>
-        </div>
-      </q-banner>
-
-      <h1 class="text-center" >Welcome To the Webpage!</h1>
-
+      <h1 class="text-center text-xl m-4 p-4" >Welcome To the Webpage!</h1>
 
     </q-page>
   `
